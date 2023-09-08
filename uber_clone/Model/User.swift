ad8e45e40b009c8b41d6c947decd42f3c4952e53
muslimerkinovich <muslimerkinovich@gmail.com>
2023-09-08 23:54:16 +0500

    - user data model

---
+++ uber_clone/Model/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  uber_clone
+//
+//  Created by Muslim on 08/09/23.
+//
+
+import Foundation
+
+struct User {
+    let fullName: String
+    let email: String
+    let accountType: Int
+    
+    init(dictionary: [String: Any]) {
+        self.fullName = dictionary["fullName"] as? String ?? ""
+        self.email = dictionary["email"] as? String ?? ""
+        self.accountType = dictionary["accountType"] as? Int ?? 0
+    }
+}
